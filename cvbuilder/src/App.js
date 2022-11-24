be62f8ae037cfda0fda5f5beda349a8ca08eb211
Saltie066 <saltie066@saltstudy.onmicrosoft.com>
2022-11-24 16:09:34 +0100

    merging with noor and swathi

--- cvbuilder/src/App.js
+++ cvbuilder/src/App.js
@@ -1,12 +1,10 @@
 import Welcome from './Components/Welcome';
-import Form from './Components/Form';
 
 function App() {
   return (
     <div className="App">
       <h1>
         <Welcome/>
-        <Form></Form>
       </h1>
     </div>
 
