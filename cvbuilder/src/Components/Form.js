c61e99ad66047bfd7530f1d32e5ed510c0e1f3a7
CrystalMahnoor <67906566+CrystalMahnoor@users.noreply.github.com>
2022-11-24 16:00:42 +0100

    Fixed

--- cvbuilder/src/Components/Form.js
+++ cvbuilder/src/Components/Form.js
@@ -1,86 +1,137 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ProfileImage from "./ProfileImage";
 import "./Form.css";
 import "./ProfileImage.css";
 
-
 function Form() {
-  const initialContact = { Name: "", Email: "", PhoneNumber: "", LinkedIn: "", Summary: "" };
-    const [info, setInfo] = useState(initialContact);
+  const initialContact = {
+    Name: "",
+    Email: "",
+    PhoneNumber: "",
+    LinkedIn: "",
+    Summary: "",
+    Education: "",
+    WorkExperince: "",
+    TechnicalSkills: "",
+  };
+  const [info, setInfo] = useState(initialContact);
   const handleChangeFor = (propertyName) => (event) => {
     setInfo((info) => ({
       ...info,
-      [propertyName]: event.target.value
+      [propertyName]: event.target.value,
     }));
-    };
-  
+  };
+
+  function onSubmit(e) {
+    e.preventDefault();
+    localStorage.setItem("Name", info.Name);
+    localStorage.setItem("Email", info.Email);
+    localStorage.setItem('PhoneNumber', info.PhoneNumber);
+    localStorage.setItem("LinkedIn", info.LinkedIn);
+    localStorage.setItem("Summary", info.Summary);
+    localStorage.setItem("Education", info.Education);
+    localStorage.setItem("WorkExperince", info.WorkExperince);
+    localStorage.setItem("TachnicalSkills", info.TechnicalSkills);
+    localStorage.setItem("Languages", info.Languages);
+    localStorage.setItem("Certificates", info.Certificates);
+  }
   return (
     <div>
       <section id="myform">
         <h2 id="title">Form</h2>
 
-        <form className="row">
-        <ProfileImage></ProfileImage>
-        <label className='labels'>Name</label>
+        <form onSubmit={onSubmit} className="row">
+          <ProfileImage></ProfileImage>
+          <label className="labels">Name</label>
           <br />
-          <input type="text"
-        onChange={handleChangeFor("Name")}
-        value={info.Name}></input>
+          <input
+            type="text"
+            onChange={handleChangeFor("Name")}
+            value={info.Name}
+          ></input>
           <br />
-          <label className='labels'>Email</label>
+          <label className="labels">Email</label>
           <br />
-          <input type="text"
-        onChange={handleChangeFor("Email")}
-        value={info.Email}></input>
+          <input
+            type="text"
+            onChange={handleChangeFor("Email")}
+            value={info.Email}
+          ></input>
           <br />
-          <label className='labels'>Phone Number</label>
+          <label className="labels">Phone Number</label>
           <br />
-          <input type="text"
-        onChange={handleChangeFor("Phone Number")}
-        value={info.PhoneNumber}></input>
+          <input
+            type="text"
+            onChange={handleChangeFor("PhoneNumber")}
+            value={info.PhoneNumber}
+          ></input>
           <br />
-          <label className='labels'>LinkedIn</label>
+
+          <label className="labels">LinkedIn</label>
           <br />
-          <input type="text"
-        onChange={handleChangeFor("LinkedIn")}
-        value={info.LinkedIn}></input>
+          <input
+            type="text"
+            onChange={handleChangeFor("LinkedIn")}
+            value={info.LinkedIn}
+          ></input>
           <br />
-          <label className='labels'>Summary</label>
+
+          <label className="labels">Summary</label>
           <br />
-          <input type="text"
-        onChange={handleChangeFor("Summary")}
-        value={info.Summary}></input>
-         
-          <label className='labels'>Education & Training</label>
+          <input
+            type="text"
+            onChange={handleChangeFor("Summary")}
+            value={info.Summary}
+          ></input>
           <br />
-          <input className='input-fields'></input>
+
+          <label className="labels">Education</label>
           <br />
-          <label className='labels'>Work Experince</label>
+          <input
+            type="text"
+            onChange={handleChangeFor("Education")}
+            value={info.Education}
+          ></input>
           <br />
-          <input className='input-fields'></input>
+          <label className="labels">Work Experince</label>
           <br />
-          <label className='labels'>Techinical Skills</label>
+          <input
+            type="text"
+            onChange={handleChangeFor("WorkExperince")}
+            value={info.WorkExperince}
+          ></input>
           <br />
-          <input className='input-fields'></input>
+          <label className="labels">Technical Skills</label>
           <br />
-          <label className='labels'>Languages</label>
+          <input
+            type="text"
+            onChange={handleChangeFor("TechnicalSkills")}
+            value={info.TechnicalSkills}
+          ></input>
           <br />
-          <input className='input-fields'></input>
+          <label className="labels">Languages</label>
           <br />
-          <label className='labels'>Links</label>
+          <input
+            type="text"
+            onChange={handleChangeFor("Languages")}
+            value={info.Languages}
+          ></input>
           <br />
-          <input className='input-fields'></input>
+          <label className="labels">Certificates</label>
           <br />
-          <label className='labels'>Certificates</label>
+          <input
+            type="text"
+            onChange={handleChangeFor("Certificates")}
+            value={info.Certificates}
+          ></input>
           <br />
-          <input className='input-fields'></input>
-          <br />
-          <button variant="primary" type="submit" id='btnAddTodo'>Submit</button>
+          <button variant="primary" type="submit" id="btnAddTodo">
+            Submit
+          </button>
         </form>
       </section>
     </div>
   );
 }
-
 
 export default Form;
