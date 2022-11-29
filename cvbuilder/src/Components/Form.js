76377db0424052573b6f1eda8c8ddf9fb8dadc90
Saltie066 <saltie066@saltstudy.onmicrosoft.com>
2022-11-29 15:41:04 +0100

    fixed resume2 and form

--- cvbuilder/src/Components/Form.js
+++ cvbuilder/src/Components/Form.js
@@ -12,7 +12,7 @@ function Form() {
     Email: "",
     PhoneNumber: "",
     LinkedIn: "",
-    Summary: "", 
+    Summary: "",
     Education: "",
     WorkExperince: "",
     TechnicalSkills: "",
@@ -44,92 +44,94 @@ function Form() {
   return (
     <div>
       <Headerform></Headerform>
-      <section id="myform">
-        <h2 id="title">Form</h2>
-        <form className="row"> 
-          {/* <ProfileImage onChange={handleChangeFor("ProfileImage")}></ProfileImage> */}
-          <label className="labels">Name</label>
-          <br />
-          <input
-            type="text" required='required'
-            onChange={handleChangeFor("Name")}
-            value={info.Name}
-          ></input>
-          <br />
-          <label className="labels">Email</label>
-          <br />
-          <input
-            type="email" required='required'
-            onChange={handleChangeFor("Email")}
-            value={info.Email}
-          ></input>
-          <br />
-          <label className="labels">Phone Number</label>
-          <br />
-          <input
-            type="text" required='required'
-            onChange={handleChangeFor("PhoneNumber")}
-            value={info.PhoneNumber}
-          ></input>
-          <br />
+      <h2 id="form-title">Form</h2>
+      <form className="form-row">
+        <article className="form-article">
+          <section className="form-section1">
+            <label className="labels1">Name</label>
+            <br />
+            <input className="form-input"
+              type="text" required='required'
+              onChange={handleChangeFor("Name")}
+              value={info.Name}
+            ></input>
+            <br />
+            <label className="labels1">Email</label>
+            <br />
+            <input className="form-input"
+              type="email" required='required'
+              onChange={handleChangeFor("Email")}
+              value={info.Email}
+            ></input>
+            <br />
+            <label className="labels1">Phone Number</label>
+            <br />
+            <input className="form-input"
+              type="text" required='required'
+              onChange={handleChangeFor("PhoneNumber")}
+              value={info.PhoneNumber}
+            ></input>
+            <br />
 
-          <label className="labels">LinkedIn</label>
-          <br />
-          <input
-            type="text"
-            onChange={handleChangeFor("LinkedIn")}
-            value={info.LinkedIn}
-          ></input>
-          <br />
+            <label className="labels1">LinkedIn</label>
+            <br />
+            <input className="form-input"
+              type="text"
+              onChange={handleChangeFor("LinkedIn")}
+              value={info.LinkedIn}
+            ></input>
+            <br />
 
-          <label className="labels">Summary</label>
-          <br />
-          <input
-            type="text" required='required'
-            onChange={handleChangeFor("Summary")}
-            value={info.Summary}
-          ></input>
-          <br />
+          </section>
 
-          <label className="labels">Education</label>
-          <br />
-          <textarea onChange={handleChangeFor("Education")} required='required'
-            value={info.Education} rows="8" cols="100"></textarea>
-          <br />
-          <label className="labels">Work Experince</label>
-          <br />
-          <textarea
-            onChange={handleChangeFor("WorkExperince")} required='required'
-            value={info.WorkExperince} rows="8" cols="100"
-          ></textarea>
-          <br />
-          <label className="labels">Skills</label>
-          <br />
-          <textarea
-            onChange={handleChangeFor("TechnicalSkills")} required='required'
-            value={info.TechnicalSkills} rows="8" cols="100"
-          ></textarea>
-          <br />
-          <label className="labels">Languages</label>
-          <br />
-          <textarea
-            onChange={handleChangeFor("Languages")} required='required'
-            value={info.Languages} rows="6" cols="100"
-          ></textarea>
-          <br />
-          <label className="labels">Certificates</label>
-          <br />
-          <textarea
-            onChange={handleChangeFor("Certificates")}
-            value={info.Certificates} rows="4" cols="100"
-          ></textarea>
-          <br />
-          <button variant="primary" type="submit" id="btnAddTodo"
-           onClick={onSubmit}>
-            Submit
-          </button>
-        </form>
-      </section>
+          <section className="form-section2">
+            <label className="labels2">Summary</label>
+            <br />
+            <textarea className="form-textarea"
+              rows="8" cols="100" required='required'
+              onChange={handleChangeFor("Summary")}
+              value={info.Summary}
+            ></textarea>
+            <br/>
+            <label className="labels2">Education</label>
+            <br />
+            <textarea onChange={handleChangeFor("Education")} required='required' className="form-textarea"
+              value={info.Education} rows="8" cols="100"></textarea>
+            <br />
+            <label className="labels2">Work Experince</label>
+            <br />
+            <textarea className="form-textarea"
+              onChange={handleChangeFor("WorkExperince")} required='required'
+              value={info.WorkExperince} rows="8" cols="100"
+            ></textarea>
+            <br />
+            <label className="labels2">Skills</label>
+            <br />
+            <textarea className="form-textarea"
+              onChange={handleChangeFor("TechnicalSkills")} required='required'
+              value={info.TechnicalSkills} rows="8" cols="100"
+            ></textarea>
+            <br />
+            <label className="labels2">Languages</label>
+            <br />
+            <textarea className="form-textarea"
+              onChange={handleChangeFor("Languages")} required='required'
+              value={info.Languages} rows="6" cols="100"
+            ></textarea>
+            <br />
+            <label className="labels2">Certificates</label>
+            <br />
+            <textarea className="form-textarea"
+              onChange={handleChangeFor("Certificates")}
+              value={info.Certificates} rows="4" cols="100"
+            ></textarea>
+          </section>
+        </article>
+        <button variant="primary" type="submit" id="form-btn"
+          onClick={onSubmit}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
