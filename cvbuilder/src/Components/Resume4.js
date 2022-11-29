d74f5b55027013db059b1dc2aa4b9fac04c2ffcd
Saltie065 <saltie065@saltstudy.onmicrosoft.com>
2022-11-29 15:39:42 +0100

    add template4

--- cvbuilder/src/Components/Resume4.js
+++ cvbuilder/src/Components/Resume4.js
@@ -1,8 +1,8 @@
 import React from "react";
-import './Resume3.css';
+import './Resume.css';
 
-function Resume3 () {
-    const name = localStorage.getItem("Name").toUpperCase();
+function Resume4 () {
+    const name = localStorage.getItem("Name");
     const email = localStorage.getItem("Email");
     const phoneNumber = localStorage.getItem("PhoneNumber");
     const linkedIn = localStorage.getItem("LinkedIn");
@@ -14,7 +14,7 @@ function Resume3 () {
     const certificates = localStorage.getItem("Certificates");
     function printpage() {
         
-        var printButton = document.getElementById("printpagebutton3");
+        var printButton = document.getElementById("printpagebutton4");
     
         printButton.style.visibility = 'hidden';
         
@@ -22,38 +22,35 @@ function Resume3 () {
         printButton.style.visibility = 'visible';
     }
     return (
-        <section id="resume3">
+        <section id="resume4">
              <style>
-              {`@media print {#resume3 {
+              {`@media print {#resume4 {
     margin: auto;
     max-width: 100%;
   }}`}
           </style>
-            <h1 className="template-heading3">{name}</h1>
-            <section className="template-section3">
-            <article className="template-content3">
-                <h4>CONTACT</h4>
+            <h1 className="cv-heading">{name}</h1>
+            <article className="cv-info">
                 <span>{email}</span><br />
                 <span>{phoneNumber}</span><br />
                 <span>{linkedIn}</span><br />
             </article>
-            <div className="template-content3"> <h4>ABOUT ME</h4>
-            <p>{summary}</p></div>
-            
-            <div className="template-content3"><h4>SKILLS</h4>
+            <p>{summary}</p>
+            <section className="cv-section">
+            <div className="cv-content"><h4>Skills</h4>
                 <p dangerouslySetInnerHTML={{ __html: technicalSkills }}/></div>
-                <div className="template-content3"><h4>EDUCATION</h4>
+                <div className="cv-content"><h4>Education</h4>
                 <p dangerouslySetInnerHTML={{ __html: education }} /></div>
-                <div className="template-content3"><h4>LANGUAGES</h4>
+                <div className="cv-content"><h4>Languages</h4>
                 <p dangerouslySetInnerHTML={{ __html: languages }}/></div>
-                <div className="template-content3"><h4>EXPERIENCE</h4>
+                <div className="cv-content"><h4>Experience</h4>
                 <p dangerouslySetInnerHTML={{ __html: workExperince }}/></div>
-                <div className="template-content3"><h4>CERTIFICATES</h4>
+                <div className="cv-content"><h4>Certificates</h4>
                 <p dangerouslySetInnerHTML={{ __html: certificates }}/></div>
             </section>
-            <button id="printpagebutton3" onClick={printpage}>PRINT</button>
+            <button id="printpagebutton4" onClick={printpage}>PRINT</button>
         </section>
     )
 }
 
-export default Resume3;
+export default Resume4;
